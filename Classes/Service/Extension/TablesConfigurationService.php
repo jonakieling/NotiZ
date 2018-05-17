4f5789681a4a04434db27d83074122a4162dcf5b
Romain Canon <romain.hydrocanon@gmail.com>
2018-05-17 16:07:38 +0200

    WIP

--- Classes/Service/Extension/TablesConfigurationService.php
+++ Classes/Service/Extension/TablesConfigurationService.php
@@ -19,7 +19,15 @@ namespace CuyZ\Notiz\Service\Extension;
 use CuyZ\Notiz\Core\Definition\Builder\DefinitionBuilder;
 use CuyZ\Notiz\Core\Definition\Tree\Definition;
 use CuyZ\Notiz\Core\Support\NotizConstants;
+use CuyZ\Notiz\Service\BackendUriBuilder;
+use CuyZ\Notiz\Service\Container;
 use CuyZ\Notiz\Service\Traits\SelfInstantiateTrait;
+use ReflectionClass;
+use TYPO3\CMS\Backend\Controller\EditDocumentController;
+use TYPO3\CMS\Backend\Template\Components\ButtonBar;
+use TYPO3\CMS\Backend\Template\ModuleTemplate;
+use TYPO3\CMS\Core\Imaging\Icon;
+use TYPO3\CMS\Core\Imaging\IconFactory;
 use TYPO3\CMS\Core\SingletonInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\SignalSlot\Dispatcher;
@@ -61,6 +69,35 @@ class TablesConfigurationService implements SingletonInterface
     {
         $this->registerBackendModule();
         $this->registerShowNotificationControllers();
+
+        $this->dispatcher->connect(
+            EditDocumentController::class,
+            'initAfter',
+            function (EditDocumentController $controller) {
+                \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($controller, __CLASS__ . ':' . __LINE__ . ' $controller');
+                $iconFactory = Container::get(IconFactory::class);
+                $backendUriBuilder = Container::get(BackendUriBuilder::class);
+
+                $reflection = new ReflectionClass($controller);
+                $property = $reflection->getProperty('moduleTemplate');
+                $property->setAccessible(true);
+
+                /** @var ModuleTemplate $moduleTemplate */
+                $moduleTemplate = $property->getValue($controller);
+
+                $buttonBar = $moduleTemplate->getDocHeaderComponent()->getButtonBar();
+
+                $test = $buttonBar->makeLinkButton()
+                    ->setShowLabelText(true)
+                    ->setHref($backendUriBuilder->uriFor('showDefinition'))
+                    ->setTitle('View details')
+                    ->setIcon($iconFactory->getIcon(
+                        'actions-view',
+                        Icon::SIZE_SMALL
+                    ));
+                $buttonBar->addButton($test, ButtonBar::BUTTON_POSITION_LEFT, -1);
+            }
+        );
     }
 
     /**
